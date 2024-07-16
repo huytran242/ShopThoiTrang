aeefa36c15e904858c8700698f6022722429480b
Phan-Viet-Hung <hungpvph51204@gmail.com>
2024-07-16 17:38:07 +0700

    hung1

---
+++ WebThoiTrang/Migrations/20240716094146_u.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebThoiTrang.Migrations
+{
+    /// <inheritdoc />
+    public partial class u : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_User_UserId",
+                table: "orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_reviews_User_UserId1",
+                table: "reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_users_UserId",
+                table: "orders",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_reviews_users_UserId1",
+                table: "reviews",
+                column: "UserId1",
+                principalTable: "users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_users_UserId",
+                table: "orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_reviews_users_UserId1",
+                table: "reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "User");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_User_UserId",
+                table: "orders",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_reviews_User_UserId1",
+                table: "reviews",
+                column: "UserId1",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
